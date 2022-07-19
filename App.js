60d1cb3c610a349bf80c4cc43e7c24a63c3f9cfa
Juan Galarce <j6alarce@gmail.com>
2022-07-19 10:59:30 -0400

    Se agregan los primeros estilos al componente

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, View, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text> Hola Mundo </Text>
-    </SafeAreaView>
+    <View style={styles.contaier}>
+      <Text style={styles.title}>
+        Administrador de Citas {''}
+        <Text style={styles.titleBold}>Veterinaria </Text>
+      </Text>
+    </View>
   );
 };
 
+const styles = StyleSheet.create({
+  contaier: {
+    backgroundColor: '#D3D3D3',
+    flex: 1,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600',
+  },
+  titleBold: {
+    fontWeight: '900',
+    color: '#6D289D',
+  },
+});
+
 export default App;
+
+/* 
+Solucion:
+
+cd android
+
+.\gradlew clean
+
+cd ..
+
+npm cache clean --force
+
+
+*/
