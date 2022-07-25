a36899dcfad8d2635a31847f4f6602db6736ce97
Juan Galarce <j6alarce@gmail.com>
2022-07-25 09:45:20 -0400

    Se le da estilos al componente de pacientes

--- src/components/Patient.js
+++ src/components/Patient.js
@@ -6,11 +6,36 @@ const Patient = ({item}) => {
   const {patient, owner, email, phone, date, symtoms} = item;
 
   return (
-    <View>
-      <Text>{patient}</Text>
-      <Text>{formatDate(date)}</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Paciente</Text>
+      <Text style={styles.text}>{patient}</Text>
+      <Text style={styles.date}>{formatDate(date)}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderBottomColor: '#94a3b8',
+    borderBottomWidth: 1,
+  },
+  label: {
+    color: '#374151',
+    textTransform: 'uppercase',
+    fontWeight: '700',
+    marginBottom: 10,
+  },
+  text: {
+    color: '#6d28d9',
+    fontSize: 24,
+    fontWeight: '700',
+    marginBottom: 10,
+  },
+  date: {
+    color: '374151',
+  },
+});
 
 export default Patient;
