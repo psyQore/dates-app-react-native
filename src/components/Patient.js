edd30324faa054b1675b6ac51501a453722cd209
Juan Galarce <j6alarce@gmail.com>
2022-07-25 09:36:37 -0400

    Se crea un helper para darle formato a la fecha

--- src/components/Patient.js
+++ src/components/Patient.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {formatDate} from '../helpers';
 
 const Patient = ({item}) => {
   const {patient, owner, email, phone, date, symtoms} = item;
-
-  const formatDate = date => {
-    const dateNew = new Date(date);
-
-    const options = {
-      weekdat: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: '2-digit',
-    };
-
-    return dateNew.toLocaleDateString('es-ES', options);
-  };
 
   return (
     <View>
