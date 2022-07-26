fe9e7f9a89af4513dbf58f61b7eb30b04576ef75
Juan Galarce <j6alarce@gmail.com>
2022-07-26 11:13:32 -0400

    Se trabaja en las sombras

--- src/components/PatientInformation.js
+++ src/components/PatientInformation.js
@@ -4,50 +4,71 @@ import {Text, View, Pressable, StyleSheet} from 'react-native';
 const PatientInformation = ({patient, setModalPatient}) => {
   const {patient: patientName, owner, email, phone, symtoms} = patient;
   return (
-    <View style={styles.content}>
-
-      <Text style={styles.title}>Información {""}
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Información {''}
         <Text style={styles.titleBold}>Paciente</Text>
       </Text>
       <View>
-        <Pressable style={styles.btnClose} onLongPress={() => setModalPatient(false)}>
+        <Pressable
+          style={styles.btnClose}
+          onLongPress={() => setModalPatient(false)}>
           <Text style={styles.btnCloseText}>X Cerrar</Text>
         </Pressable>
       </View>
-      <Text>{patientName}</Text>
+
+      <View style={styles.content}>
+        <Text>{patientName}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    content: {
-        backgroundColor: "#f59E0B",
-        flex: 1
+  container: {
+    backgroundColor: '#f59E0B',
+    flex: 1,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '600',
+    textAlign: 'center',
+    marginTop: 30,
+    color: '#FFF',
+  },
+  titleBold: {
+    fontWeight: '900',
+  },
+  btnClose: {
+    marginVertical: 30,
+    backgroundColor: '#E06900',
+    marginHorizontal: 30,
+    padding: 20,
+    borderRadius: 10,
+  },
+  btnCloseText: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontWeight: '900',
+    fontSize: 16,
+    textTransform: 'uppercase',
+  },
+  content: {
+    backgroundColor: '#FFF',
+    marginHorizontal: 30,
+    borderRadius: 10,
+    padding: 10,
+    height: 300,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
     },
-    title: {
-        fontSize: 30,
-        fontWeight: '600',
-        textAlign: 'center',
-        marginTop: 30,
-        color: '#FFF',
-      },
-      titleBold: {
-        fontWeight: '900',
-      },
-      btnClose: {
-        marginVertical: 30,
-        backgroundColor: '#E06900',
-        marginHorizontal: 30,
-        padding: 20,
-        borderRadius: 10,
-      },
-      btnCloseText: {
-        color: '#FFF',
-        textAlign: 'center',
-        fontWeight: '900',
-        fontSize: 16,
-        textTransform: 'uppercase',
-      },
-})
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+
+    elevation: 11,
+  },
+});
 
 export default PatientInformation;
